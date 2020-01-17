566df1f5fe291374595ac27c61849fbc6a471497
Phil King <phil@beardedphil.com>
2020-01-17 13:27:27 -0600

    .gitignore fix

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,9 +1,11 @@
+from django.db.models import OuterRef, Subquery, Count
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
-from .models import Choice, Question
+from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
@@ -11,18 +13,83 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        response = Question.objects.annotate(
+                choice_count=Subquery(
+                    Choice.objects.filter(question=OuterRef('pk'))
+                    .values('question')
+                    .annotate(choice_count=Count('pk'))
+                    .values('choice_count')
+                )
+            ).filter(
+                choice_count__gte=2,
+            )
+
+        if self.request.user.is_staff:
+            return response.order_by('-pub_date')[:5]
+        else:
+            return response.filter(
+                pub_date__lte=timezone.now(),
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        Excludes any questions with zero or one choice.
+        """
+        response = Question.objects.annotate(
+            choice_count=Subquery(
+                Choice.objects.filter(question=OuterRef('pk'))
+                .values('question')
+                .annotate(choice_count=Count('pk'))
+                .values('choice_count')
+            )
+        ).filter(
+            choice_count__gte=2,
+        )
+
+        if self.request.user.is_staff:
+            return response
+        else:
+            return response.filter(
+                pub_date__lte=timezone.now(),
+            )
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        Excludes any questions with zero or one choice.
+        """
+        response = Question.objects.annotate(
+            choice_count=Subquery(
+                Choice.objects.filter(question=OuterRef('pk'))
+                .values('question')
+                .annotate(choice_count=Count('pk'))
+                .values('choice_count')
+            )
+        ).filter(
+            choice_count__gte=2
+        )
+
+        if self.request.user.is_staff:
+            return response
+        else:
+            return response.filter(
+                pub_date__lte=timezone.now(),
+            )
 
 
 def vote(request, question_id):
@@ -39,6 +106,6 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
+        # with POST data.  This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
